62398a220eb406221ad2ca8db76010ad5085a6a5
brendanmfarrell <brendan.farrell@wework.com>
2018-02-13 15:31:18 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,17 @@
 def fizzbuzz(int)
-  if int % 3 == 0 && int % 5 == 0
-  "FizzBuzz"
-  elsif int % 3 == 0
+  if int % 3 == 0
   "Fizz"
-  elsif int % 5 == 0
+  if int % 5 == 0
   "Buzz"
-  end
 end
+
+
+#def fizzbuzz(int)
+ # if int % 3 == 0 && int % 5 == 0
+  #"FizzBuzz"
+  #elsif int % 3 == 0
+  #"Fizz"
+  #elsif int % 5 == 0
+  #"Buzz"
+  #end
+#end
